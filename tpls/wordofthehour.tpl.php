ca3f92bb4b102507af12a766576b37eb726c65dc
unknown <mikehipps@gmail.com>
2012-11-29 20:08:19 -0500

    rewrite as drupal 7 module

---
+++ tpls/wordofthehour.tpl.php
@@ -0,0 +1,9 @@
+<div id="wordofthehour">
+  <div class="wordofthehour-word">
+    <?php print $word ?>
+  </div>
+  <div class="wordofthehour-definition">
+    <?php print $definition ?>
+  </div>
+</div>
+
